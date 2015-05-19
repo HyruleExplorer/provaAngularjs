de9ba087842b31d56a75c80eb93b1d176c4c8c10
HyruleExplorer <simonecinardo@gmail.com>
2015-05-19 10:42:18 +0200

    sezione 3 del corso Angular. Form con ng-controller per aggiungere la recensione.

--- js/app.js
+++ js/app.js
@@ -24,80 +24,96 @@
         }
     );
 
+    app.controller(
+        "reviewController",
+        function () {
+            this.review = {};
+            this.addReview = function (product) {
+                product.recensioni.push(this.review);
+                this.review = {};
+            };
+        }
+    );
+
     var macchine = [
         {
-            marca : "Lamborghini",
-            modello : "Gallardo",
-            descrizione : "Una velocissima e bellissima macchina sportiva.",
-            prezzo : 350000,
-            colore : "giallo",
-            images : [
+            marca: "Lamborghini",
+            modello: "Gallardo",
+            descrizione: "Una velocissima e bellissima macchina sportiva.",
+            prezzo: 350000,
+            colore: "giallo",
+            images: [
                 {
-                    full : "img/gallardo_full.jpg",
-                    thumb : "img/gallardo_thumb.jpg"
+                    full: "img/gallardo_full.jpg",
+                    thumb: "img/gallardo_thumb.jpg"
                 }
             ],
-            canPurchase : true,
-            isSoldOut : false,
-            recensioni : [
+            canPurchase: true,
+            isSoldOut: false,
+            recensioni: [
                 {
-                    voto : 10,
-                    commento : "Macchina stupenda!"
+                    voto: 10,
+                    commento: "Macchina stupenda!",
+                    autore: "tizio@gmail.com"
                 },
                 {
-                    voto : 9,
-                    commento : "Sono felice del mio acquisto."
+                    voto: 9,
+                    commento: "Sono felice del mio acquisto.",
+                    autore: "tizio@gmail.com"
                 }
             ]
         },
         {
-            marca : "Ferrari",
-            modello : "Enzo",
-            descrizione : "Una veloce ed elegante macchina sportiva italiana. Nel rispetto della tradizione",
-            prezzo : 230000,
-            colore : "rosso",
-            images : [
+            marca: "Ferrari",
+            modello: "Enzo",
+            descrizione: "Una veloce ed elegante macchina sportiva italiana. Nel rispetto della tradizione",
+            prezzo: 230000,
+            colore: "rosso",
+            images: [
                 {
-                    full : "img/ferrari_full.jpg",
-                    thumb : "img/ferrari_thumb.jpg"
+                    full: "img/ferrari_full.jpg",
+                    thumb: "img/ferrari_thumb.jpg"
                 }
             ],
-            canPurchase : true,
-            isSoldOut : false,
-            recensioni : [
+            canPurchase: true,
+            isSoldOut: false,
+            recensioni: [
                 {
-                    voto : 9,
-                    commento : "Eccezionale! Non ve ne pentirete."
+                    voto: 9,
+                    commento: "Eccezionale! Non ve ne pentirete.",
+                    autore: "tizio@gmail.com"
                 },
                 {
-                    voto : 8.5,
-                    commento : "Notevole. Un pò dura nelle curve, però."
+                    voto: 8.5,
+                    commento: "Notevole. Un pò dura nelle curve, però.",
+                    autore: "tizio@gmail.com"
                 }
             ]
         },
         {
-            marca : "Fiat",
-            modello : "500",
-            descrizione :
-                "Una bella utilitaria italiana.Perfetta per la città",
-            prezzo : 30000,
-            colore : "bianco",
-            images : [
+            marca: "Fiat",
+            modello: "500",
+            descrizione: "Una bella utilitaria italiana.Perfetta per la città",
+            prezzo: 30000,
+            colore: "bianco",
+            images: [
                 {
-                    full : "img/fiat_full.jpg",
-                    thumb : "img/fiat_thumb.jpg"
+                    full: "img/fiat_full.jpg",
+                    thumb: "img/fiat_thumb.jpg"
                 }
             ],
-            canPurchase : true,
-            isSoldOut : false,
-            recensioni : [
+            canPurchase: true,
+            isSoldOut: false,
+            recensioni: [
                 {
-                    voto : 7,
-                    commento : "E' quello che ci si aspetta da un'utilitaria."
+                    voto: 7,
+                    commento: "E' quello che ci si aspetta da un'utilitaria.",
+                    autore: "tizio@gmail.com"
                 },
                 {
-                    voto : 6,
-                    commento : "Un pò piccola, ma non si può pretendere molto."
+                    voto: 6,
+                    commento: "Un pò piccola, ma non si può pretendere molto.",
+                    autore: "tizio@gmail.com"
                 }
             ]
         }
@@ -105,5 +121,3 @@
 
 
 }());
-
-
